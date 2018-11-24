ba4656c654279ece20199363a65a293c6c503615
Cameron Tacklind <cameron@tacklind.com>
2018-11-23 16:25:58 -0800

    Error if called with no arguments

--- tests/smallest-power-of-two.ts
+++ tests/smallest-power-of-two.ts
@@ -3,3 +3,8 @@ import smallestPowerOfTwo from '../src/smallest-power-of-two';
 test('Function loads', () => {
   expect(typeof smallestPowerOfTwo).toBe('function');
 });
+
+test('Called with no arguments', () => {
+  const f = smallestPowerOfTwo as () => number;
+  expect(f).toThrowError(TypeError('Invalid type: undefined'));
+});
